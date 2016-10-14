b6bf99aab41f0bdc8c8f09836a7bd71d5bf9889b
user117455 <user117455@mac169.local>
2016-10-14 00:58:59 -0400

    Initial Commit

---
+++ HansGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HansGit
+//
+//  Created by user117455 on 10/14/16.
+//  Copyright © 2016 user117455. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
